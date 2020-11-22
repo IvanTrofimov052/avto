30b96bb200d25c50deee8d05d5d4a25ba476f78f
IvanTrofimov052 <55573197+IvanTrofimov052@users.noreply.github.com>
2020-11-22 17:59:59 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# manual
+
+"""
+This script allows you to manually control the simulator or Duckiebot
+using the keyboard arrows.
+"""
+from PIL import Image
+import argparse
+import sys
+
+import gym
+import numpy as np
+import pyglet
+from pyglet.window import key
+
+from gym_duckietown.envs import DuckietownEnv
+
+# from experiments.utils import save_img
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--env-name", default=None)
+parser.add_argument("--map-name", default="udem1")
+parser.add_argument("--distortion", default=False, action="store_true")
+parser.add_argument("--camera_rand", default=False, action="store_true")
+parser.add_argument("--draw-curve", action="store_true", help="draw the lane following curve")
+parser.add_argument("--draw-bbox", action="store_true", help="draw collision detection bounding boxes")
+parser.add_argument("--domain-rand", action="store_true", help="enable domain randomization")
+parser.add_argument("--dynamics_rand", action="store_true", help="enable dynamics randomization")
+parser.add_argument("--frame-skip", default=1, type=int, help="number of frames to skip")
+parser.add_argument("--seed", default=1, type=int, help="seed")
+args = parser.parse_args()
+
+if args.env_name and args.env_name.find("Duckietown") != -1:
+    env = DuckietownEnv(
+        seed=args.seed,
+        map_name=args.map_name,
+        draw_curve=args.draw_curve,
+        draw_bbox=args.draw_bbox,
+        domain_rand=args.domain_rand,
+        frame_skip=args.frame_skip,
+        distortion=args.distortion,
+        camera_rand=args.camera_rand,
+        dynamics_rand=args.dynamics_rand,
+    )
+else:
+    env = gym.make(args.env_name)
+
+env.reset()
+env.render()
+
+
+@env.unwrapped.window.event
+def on_key_press(symbol, modifiers):
+    """
+    This handler processes keyboard commands that
+    control the simulation
+    """
+
+    if symbol == key.BACKSPACE or symbol == key.SLASH:
+        print("RESET")
+        env.reset()
+        env.render()
+    elif symbol == key.PAGEUP:
+        env.unwrapped.cam_angle[0] = 0
+    elif symbol == key.ESCAPE:
+        env.close()
+        sys.exit(0)
+
+    # Take a screenshot
+    # UNCOMMENT IF NEEDED - Skimage dependency
+    # elif symbol == key.RETURN:
+    #     print('saving screenshot')
+    #     img = env.render('rgb_array')
+    #     save_img('screenshot.png', img)
+
+
+# Register a keyboard handler
+key_handler = key.KeyStateHandler()
+env.unwrapped.window.push_handlers(key_handler)
+
+
+def update(dt):
+    """
+    This function is called at every frame to handle
+    movement/stepping and redrawing
+    """
+    wheel_distance = 0.102
+    min_rad = 0.08
+
+    action = np.array([0.0, 0.0])
+
+    line_pos = env.get_lane_pos2(env.cur_pos, env.cur_angle).angle_rad
+    print(line_pos)
+
+    action[0] = 0.44
+    action[1] = 20*line_pos 
+
+    obs, reward, done, info = env.step(action)
+    print("step_count = %s, reward=%.3f" % (env.unwrapped.step_count, reward))
+
+    
+    
+ 
+
+    # v2 = -10
+    
+
+    if key_handler[key.RETURN]:
+
+        im = Image.fromarray(obs)
+
+        im.save("screen.png")
+
+    if done:
+        print("done!")
+        env.reset()
+        env.render()
+
+    env.render()
+
+
+pyglet.clock.schedule_interval(update, 1.0 / env.unwrapped.frame_rate)
+
+# Enter main event loop
+pyglet.app.run()
+
+env.close()
